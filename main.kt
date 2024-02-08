c15cf4f47959cb9af4f26aa0bf3e99e16ff56e77
Nimoh-joy <wachiraglorywairimu@gmail.com>
2024-02-08 20:43:18 +0300

    created a new file

---
+++ main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var fullName="Glory Joy Wachira"
+    var age= 20
+    var phoneNumber="0768344933"
+    var weightKg=50.8F
+    var citizen :Boolean = true
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weightKg)
+    println(citizen)
+}
