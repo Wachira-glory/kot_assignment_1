999f7257325ecb6c6492d550779e2a406a6cec1a
Nimoh-joy <wachiraglorywairimu@gmail.com>
2024-02-08 23:23:54 +0300

    created a new file

--- main.kt
+++ main.kt
@@ -3,10 +3,10 @@ fun main(){
     var age= 20
     var phoneNumber="0768344933"
     var weightKg=50.8F
-    var citizen :Boolean = true
+    var kenyan  = true
     println(fullName)
     println(age)
     println(phoneNumber)
     println(weightKg)
-    println(citizen)
+    println(kenyan)
 }
